e9cac731c42fe0f060a6cf0045beca61845f6594
Mirko Krause <mirko.krause@student.hpi.de>
2019-01-30 16:42:51 +0100

    at least it compiles

--- src/main/scala/one/codebase/WordCount.scala
+++ src/main/scala/one/codebase/WordCount.scala
@@ -1,30 +1,15 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package one.codebase
 
 import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.scala._
 
 /**
  * Implements a streaming windowed version of the "WordCount" program.
- * 
+ *
  * This program connects to a server socket and reads strings from the socket.
- * The easiest way to try this out is to open a text sever (at port 12345) 
+ * The easiest way to try this out is to open a text sever (at port 12345)
  * using the ''netcat'' tool via
  * {{{
  * nc -l 12345
@@ -54,17 +39,17 @@ object WordCount {
         return
       }
     }
-    
+
     // get the execution environment
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-    
+
     // get input data by connecting to the socket
     val text: DataStream[String] = env.socketTextStream(hostname, port, '\n')
 
-    // parse the data, group it, window it, and aggregate the counts 
+    // parse the data, group it, window it, and aggregate the counts
     val windowCounts = text
           .flatMap { w => w.split("\\s") }
-          .map { w => WordWithCount(w, 1) }
+          .map { w => WordWithCount(w, 1)}
           .keyBy("word")
           .timeWindow(Time.seconds(5))
           .sum("count")
