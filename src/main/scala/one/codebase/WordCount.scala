5d2f4a2ed487705a9e10abf8102d7416ef64104b
Mirko Krause <mirko.krause@student.hpi.de>
2019-01-30 17:29:30 +0100

    reformatted

--- src/main/scala/one/codebase/WordCount.scala
+++ src/main/scala/one/codebase/WordCount.scala
@@ -8,16 +8,14 @@ import org.apache.flink.streaming.api.windowing.time.Time
 
 object WordCount {
 
-  /** Main program method */
-  def main(args: Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
 
 
-      val params = ParameterTool.fromArgs(args)
-      val inputFile = params.get("input", "tolstoy-war-and-peace.txt")
-      val outputFile = params.get("output", "count.csv")
+    val params = ParameterTool.fromArgs(args)
+    val inputFile = params.get("input", "tolstoy-war-and-peace.txt")
+    val outputFile = params.get("output", "count.csv")
 
 
-    
     // get the execution environment
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime)
@@ -26,20 +24,21 @@ object WordCount {
     val text = env.readTextFile(inputFile)
     // parse the data, group it, window it, and aggregate the counts 
     val windowCounts = text
-          .flatMap { w => w.split("\\s") }
-          .map { w => WordWithCount(w.toLowerCase().replaceAll("[^A-Za-z]", ""), 1)}
-          .filter(w => w.word != "")
-          .keyBy("word")
+      .flatMap { w => w.split("\\s") }
+      .map { w => WordWithCount(w.toLowerCase().replaceAll("[^A-Za-z]", ""), 1) }
+      .filter(w => w.word != "")
+      .keyBy("word")
+      .timeWindow(Time.days(1)).sum(1)
 
-        .timeWindow(Time.days(1)).sum(1)
-    windowCounts.writeAsCsv(outputFile).setParallelism(1)
 
     // print the results with a single thread, rather than in parallel
     windowCounts.print().setParallelism(1)
+    windowCounts.writeAsCsv(outputFile).setParallelism(1)
 
     env.execute("WordCount")
   }
 
   /** Data type for words with count */
   case class WordWithCount(word: String, count: Long)
+
 }
